2598461e6702eaa9b03b3974d6435f7f2357e89e
David Mendoza <damenpa@gmail.com>
2026-03-23 23:05:09 -0600

    corregi precio_venta en medicamento procedure

--- create-procedure-template.sql
+++ create-procedure-template.sql
@@ -44,7 +44,7 @@ CREATE PROCEDURE anadir_medicamento(
     IN M_GRAMAJE VARCHAR(50)
 )
 BEGIN
-    INSERT INTO medicamento(nombre, precio, cantidad, gramaje)
+    INSERT INTO medicamento(nombre, precio_venta, cantidad, gramaje)
     VALUES (M_NOMBRE, M_PRECIO, M_CANTIDAD, M_GRAMAJE);
 END $$
 
